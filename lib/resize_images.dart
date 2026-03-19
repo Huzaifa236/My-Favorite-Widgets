c15304b2ee488e5fa0829c2c3bdb6cc2fcdb9cd2
Huzaifa236 <105035778+Huzaifa236@users.noreply.github.com>
2026-03-19 09:27:06 +0500

    Create resize_images.dart

---
+++ lib/resize_images.dart
@@ -0,0 +1,16 @@
+//Resize JPEG 
+//import 'package:flutter_bicubic_resize/flutter_bicubic_resize.dart';
+
+//final resized = BicubicResizer.resizeJpeg(
+//  jpegBytes: originalBytes,
+//  outputWidth: 224,
+//  outputHeight: 224,
+//  quality: 95, // optional, default 95
+//);
+//Resize PNG 
+//final resized = BicubicResizer.resizePng(
+//  pngBytes: originalBytes,
+//  outputWidth: 224,
+//  outputHeight: 224,
+//  compressionLevel: 6, // optional, 0-9 (default: 6)
+// );
